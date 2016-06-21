85fb88b64cf6949c24c1b5620044304ac2d332d1
Jacob Schwartz <jacob@Jacobs-MacBook-Pro.local>
2016-06-20 21:31:49 -0700

    Added articles controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show' );
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+
+<body>
+
+<div class="container">
+    @yield('content')
+</div>
+
+@yield('footer')
+
+</body>
+
+</html>
