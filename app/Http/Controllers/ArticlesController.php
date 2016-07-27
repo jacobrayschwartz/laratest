a9d7ac61efca59f370d481dafe164949abc14b71
Jacob Schwartz <perpes.gaudium@gmail.com>
2016-07-27 08:37:03 -0700

    Adding route model binding

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -22,10 +22,7 @@ class ArticlesController extends Controller
         return view('articles.index', compact('articles'));
     }
 
-    public function show($id){
-        $article = Article::findOrFail($id);
-
-        
+    public function show(Article $article){
         return view('articles.show', compact('article'));
     }
 
@@ -47,13 +44,11 @@ class ArticlesController extends Controller
         return redirect('articles');
      }
 
-    public function edit($id){
-        $article = Article::findOrFail($id);
+    public function edit(Article $article){
         return view('articles.edit', compact('article'));
     }
 
-    public function update($id, ArticleRequest $request ){
-        $article = Article::findOrFail($id);
+    public function update(Article $article, ArticleRequest $request ){
 
         $article->update($request->all);
 
